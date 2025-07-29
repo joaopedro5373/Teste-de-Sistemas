5647432f340e2e855076e7d4e475687f064eccd0
joaopedro5373 <pjoao85373@gmail.com>
2025-07-29 20:36:44 -0300

    Update functions.py
    Adicionei novas funções.

--- functions.py
+++ functions.py
@@ -32,3 +32,29 @@ def produto(a:int, b:int)->int:
     a = round(a)
     b = round(b)
     return a*b
+
+def formatar_nome(nome:str, sobrenome:str)->str:
+    if(type(nome) == str and type(sobrenome) == str):
+        if(len(nome) == 0 or nome.isspace() or len(sobrenome) == 0 or sobrenome.isspace()):
+            raise ValueError("Os campos nome e sobrenome não podem estar vazios.")
+    else: raise TypeError("Nome e sobrenome devem ser do tipo string.")
+    nomeCompleto = f"{nome} {sobrenome}"
+    return nomeCompleto
+
+# | =============================== |
+# | Verificador de senhas funcional |
+# | =============================== |
+def verificar_senha(senha:str)->bool:
+    if(type(senha) is not str):
+        raise TypeError("A senha precisa ser uma string.")
+    elif(len(senha) == 0 or senha.isspace()):
+        raise ValueError("A senha não pode ser vazia.")
+    if(len(senha) < 8):
+        return False
+    temMaiuscula = any(c.isupper() for c in senha)
+    temMinuscula = any(c.islower() for c in senha)
+    temNumero = any(c.isdigit() for c in senha)
+    tem_especial = any(not c.isalnum() for c in senha)
+    if(temMaiuscula and temMinuscula and temNumero and tem_especial):
+        return True
+    else: return False
